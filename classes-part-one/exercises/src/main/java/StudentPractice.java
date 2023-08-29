6b9b170772a1a1e51572172ed63846614b5a3377
AceHagar <andyhaag@gmail.com>
2023-08-29 09:40:58 -0500

    "reviw AREA of circle studio"

--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,7 @@
 public class StudentPractice {
     public static void main(String[] args){
-        //insantiate your Student class below
+        //instantiate your Student class below
+        Student aStudent;
+        aStudent = new Student("Andy", 12345, 1, 4.0);
     }
 }
--- datatypes/datatypes-studio/src/main/java/org/launchcode/Circle.java
+++
@@ -1,9 +0,0 @@
-package org.launchcode;
-
-
-public class Circle {
-    public static double getArea(Double radius){
-        double PI = 3.14;
-        return PI * radius * radius;
-    }
-}
